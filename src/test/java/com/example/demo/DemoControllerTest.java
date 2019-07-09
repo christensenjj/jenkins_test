6e12ee90be6c7377afb34ae0fcbd7f40ca357406
Christensen <212763302@logon.ds.ge.com>
2019-07-09 09:00:54 -0500

    updated Tests

--- src/test/java/com/example/demo/DemoControllerTest.java
+++ src/test/java/com/example/demo/DemoControllerTest.java
@@ -12,6 +12,13 @@ public class DemoControllerTest {
         Greeting testGreeting = new Greeting(1,"Hello, Jacob!");
         assertEquals(testGreeting.getId(), controller.greeting("Jacob").getId());
         assertEquals(testGreeting.getContent(), controller.greeting("Jacob").getContent());
+    }
 
+    @Test
+    public void checkList() {
+        DemoController controller = new DemoController();
+        Greeting testGreeting = new Greeting(10,"CheckList");
+        assertEquals(testGreeting.getId(), controller.CheckList(10).getId());
+        assertEquals(testGreeting.getContent(), controller.CheckList(0).getContent());
     }
 }
